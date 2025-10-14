06bdbf9f90dce8544c56b11f3ae7368c5c2b4241
Remenby31 <baptiste.cruvellier31@gmail.com>
2025-10-14 09:02:58 +0200

    Ajout de l'intégration de Vercel Analytics dans l'application

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import GAReporter from "./components/GAReporter";
 import NotFound from "./pages/NotFound";
@@ -38,6 +39,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
         <GAReporter />
+        <VercelAnalytics />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
